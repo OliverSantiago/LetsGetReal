0d11ed70d1100e752200dc615268c30cb75a8897
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-11-22 18:47:34 -0500

    Tester

--- Tester.java
+++ Tester.java
@@ -2,12 +2,17 @@ public class Tester{
   public static void main(String[]args){
     RealNumber test = new RealNumber(3.0);
     RealNumber test2 = new RealNumber(5.0);
+    RationalNumber test3 = new RationalNumber(1,2);
+    RationalNumber test4 = new RationalNumber(5,10);
 
+    // System.out.println(test.equals(test2));
+    // System.out.println(test.add(test2));
+    // System.out.println(test.multiply(test2));
+    // System.out.println(test.divide(test2));
+    // System.out.println(test.subtract(test2));
 
-    System.out.println(test.equals(test2));
-    System.out.println(test.add(test2));
-    System.out.println(test.multiply(test2));
-    System.out.println(test.divide(test2));
-    System.out.println(test.subtract(test2));
+    System.out.println(test3.getNumerator());
+    System.out.println(test3.getDenominator());
+    System.out.println(test3.getValue());
   }
 }
