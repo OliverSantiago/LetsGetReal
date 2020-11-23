cb9e14e6dcdd70cde9b20d8495649699fb53e11f
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-11-22 20:22:02 -0500

    tests

--- Tester.java
+++ Tester.java
@@ -4,7 +4,7 @@ public class Tester{
     RealNumber test2 = new RealNumber(5.0);
     RationalNumber test3 = new RationalNumber(1,2);
     RationalNumber test4 = new RationalNumber(5,10);
-    RationalNumber test5 = new RationalNumber(1,2);
+    RationalNumber test5 = new RationalNumber(4,7);
 
     // System.out.println(test.equals(test2));
     // System.out.println(test.add(test2));
@@ -16,14 +16,18 @@ public class Tester{
     // System.out.println(test3.getDenominator());
     // System.out.println(test3.getValue());
 
-    System.out.println(test3.reciprocal().getValue());
-    System.out.println(test3.equals(test4));
-    System.out.println(test3.equals(test5));
-    System.out.println(test3.toString());
-
-    System.out.println(test4.gcdtester());
-
-    System.out.println(test4.toString());
-
+    // System.out.println(test3.reciprocal().getValue());
+    // System.out.println(test3.equals(test4));
+    // System.out.println(test3.equals(test5));
+    // System.out.println(test3.toString());
+    //
+    // System.out.println(test4.gcdtester());
+    //
+    // System.out.println(test4.toString());
+    //
+    // System.out.println(test3.multiply(test5).toString());
+    // System.out.println(test3.divide(test5).toString());
+    System.out.println(test3.add(test5).toString());
+    System.out.println(test3.subtract(test5).toString());
   }
 }
