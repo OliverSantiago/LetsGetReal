321d42ed35b36f39871cacecd13ae63ff94b5a75
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-11-22 19:10:26 -0500

    New method

--- Tester.java
+++ Tester.java
@@ -4,6 +4,7 @@ public class Tester{
     RealNumber test2 = new RealNumber(5.0);
     RationalNumber test3 = new RationalNumber(1,2);
     RationalNumber test4 = new RationalNumber(5,10);
+    RationalNumber test5 = new RationalNumber(1,2);
 
     // System.out.println(test.equals(test2));
     // System.out.println(test.add(test2));
@@ -11,8 +12,12 @@ public class Tester{
     // System.out.println(test.divide(test2));
     // System.out.println(test.subtract(test2));
 
-    System.out.println(test3.getNumerator());
-    System.out.println(test3.getDenominator());
-    System.out.println(test3.getValue());
+    // System.out.println(test3.getNumerator());
+    // System.out.println(test3.getDenominator());
+    // System.out.println(test3.getValue());
+
+    System.out.println(test3.reciprocal().getValue());
+    System.out.println(test3.equals(test4));
+    System.out.println(test3.equals(test5));
   }
 }
