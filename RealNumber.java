ad35984e6aeaa42e87347413efaf93d2451dc84f
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-11-23 18:58:33 -0500

    changed method

--- RealNumber.java
+++ RealNumber.java
@@ -1,4 +1,4 @@
-public class RealNumber{
+public class RealNumber extends Number{
   private double value;
 
   public RealNumber(double v){
@@ -18,15 +18,15 @@ public class RealNumber{
   *Return true when the values are within 0.001% of eachother.
   *Special case: if one is exactly zero, the other must be exactly zero.
   */
-  public boolean equals(RealNumber other1){
-    double other = other1.getValue();
-    RealNumber zero = new RealNumber(0.0);
-    if (value == 0.0){
-       return (other==0.0);
-    }
-    double temp = (Math.abs(value-other))/((value+other)/2)*100;
-    return (temp<0.001);
-  }
+  // public boolean equals(RealNumber other1){
+  //   double other = other1.getValue();
+  //   RealNumber zero = new RealNumber(0.0);
+  //   if (value == 0.0){
+  //      return (other==0.0);
+  //   }
+  //   double temp = (Math.abs(value-other))/((value+other)/2)*100;
+  //   return (temp<0.001);
+  // }
 
   /*
   *Return a new RealNumber that has the value of:
